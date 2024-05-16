97dfc22c1508d3abfbeeb932e9bd69cfefa1fc7f
Gourab Paul <gourabpaul900@gmail.com>
2024-05-16 21:34:34 +0530

    final uwu

--- frontend-shadcn/src/screens/dashboard.tsx
+++ frontend-shadcn/src/screens/dashboard.tsx
@@ -34,18 +34,36 @@ import {
 } from "@/components/ui/tooltip";
 import { Separator } from "@/components/ui/separator";
 import { useState } from "react";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+
+interface Source {
+  name: string;
+  section: string;
+  url: string;
+}
+
+interface AIMessage {
+  text: string;
+  references: Source[];
+}
 
 interface Chat {
   user: string;
   msg: string;
+  sources: Source[]
 }
 
 export function Dashboard() {
   
   const [currentUserMsg, setCurrentUserMsg] = useState("hello");
-  const [currentAIMsg, setCurrentAIMsg] = useState("");
+  const [currentAIMsg, setCurrentAIMsg] = useState<AIMessage>({text: "Hello, how may i help you today?", references: []});
   const [questionType, setQuestionType] = useState("answer");
-  const [chatHistory, setChatHistory] = useState<Chat[]>([{user:"ai", msg: "Hello, how can I help you?"},{user:"human", msg: "Hello, how can I help you?"}]);
+  const [references, setReferences] = useState("answer");
+  const [chatHistory, setChatHistory] = useState<Chat[]>([
+    {user:"ai", msg: "Hello, how can I help you?", sources:[]},
+    {user:"human", msg: "Hello, how can I help you?", sources:[]},
+  ]);
   
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<any>(null);
@@ -68,8 +86,10 @@ export function Dashboard() {
         throw new Error('Network response was not ok');
       }
       const responseData = await response.json();
-      setCurrentAIMsg(responseData);
-      setChatHistory([...chatHistory, {user:"ai", msg: responseData}]);
+      console.log(responseData)
+      setReferences(responseData.references)
+      setCurrentAIMsg({text: responseData.text, references: responseData.references });
+      setChatHistory([...chatHistory, {user:"ai", msg: responseData.text, sources:[]}]);
     } catch (error) {
       setError(error);
     } finally {
@@ -82,29 +102,39 @@ export function Dashboard() {
     <div className="grid h-screen w-full">
       <div className="flex flex-col">
         <main className="grid flex-1 gap-4 overflow-auto md:grid-cols-2 lg:grid-cols-5">
-          <div className="relative flex h-full min-h-[50vh] flex-col bg-muted/50 p-4 lg:col-span-4">
+          <div className="relative flex h-full min-h-[50vh] flex-col bg-muted/50 p-4 lg:col-span-4 max-h-screen px-36">
             <Badge variant="outline" className="absolute right-3 top-3">
               Output
             </Badge>
             
-          <div className="flex-1" />
-            {/* <div className="min-h-12 resize-none border-0 p-3 shadow-none focus-visible:ring-0" style={{backgroundColor:"#333"}}>
-              fgsdgs
-            </div>
-            <div className="min-h-12 resize-none border-0 p-3 shadow-none focus-visible:ring-0" style={{backgroundColor:"#333"}}>
-              fgsdgs
-            </div> */}
-            {chatHistory && chatHistory.map((chat, index) => {
-              return(
-                chat.user == "ai" ?
-                <div key={chat+index.toString()} className="min-h-12 resize-none border-0 p-3 shadow-none focus-visible:ring-0">
-                  {chat.msg}
-                </div>
-                :
-                <div key={chat+index.toString()} className="min-h-12 resize-none border-0 p-3 shadow-none focus-visible:ring-0 content-end">
-                  {chat.msg}
-                </div>
-            )})}
+          {/* <div className="flex-1" /> */}
+            
+            <ScrollArea className="h-screen pt-8">
+              <div className="min-h-12 resize-none border-0 p-3 shadow-none focus-visible:ring-0 leading-loose" 
+                // style={{backgroundColor:"#9abcde"}}
+              >
+                {currentAIMsg && currentAIMsg.text}
+              </div>
+              {/* {chatHistory &&
+                chatHistory.map((chat, index) => {
+                  return chat.user == "ai" ? (
+                    <div
+                      key={chat + index.toString()}
+                      className="min-h-12 resize-none border-0 p-3 shadow-none focus-visible:ring-0"
+                    >
+                      {chat.msg}
+                    </div>
+                  ) : (
+                    <div
+                      key={chat + index.toString()}
+                      className="min-h-12 resize-none border-0 p-3 shadow-none focus-visible:ring-0 content-end"
+                    >
+                      {chat.msg}
+                    </div>
+                  );
+                })} */}
+            </ScrollArea>
+            
             <form
               className="relative overflow-hidden rounded-lg border bg-background focus-within:ring-1 focus-within:ring-ring"
               x-chunk="dashboard-03-chunk-1"
@@ -118,7 +148,7 @@ export function Dashboard() {
                 className="min-h-12 resize-none border-0 p-3 shadow-none focus-visible:ring-0"
                 onChange={(e) => setCurrentUserMsg(e.target.value)}
               />
-              <div className="flex items-center p-3 pt-0">
+              <div className="flex items-center p-3 pb-10 pt-0">
                 {/* <TooltipProvider>
                   <Tooltip>
                     <TooltipTrigger asChild>
@@ -190,8 +220,61 @@ export function Dashboard() {
                     </SelectContent>
                   </Select>
                 </div>
+                <Separator />
+
+                <div className="grid gap-3 pr-4">
+                  <legend className="-ml-1 px-1 text-lg font-bold pb-4">
+                    Sources
+                  </legend>
+                  <ScrollArea>
+                    {currentAIMsg && currentAIMsg.references.length > 0 && currentAIMsg.references.map((source, index) => {
+                      return (<>
+                        <div style={{
+                          // backgroundColor:"#9abcde",
+                          padding:"10px",
+                          margin:"0px 10px 10px 0px",
+                          borderRadius:"10px",
+                          border:"1px solid #999",
+                          // maxWidth:"inherit",
+                        }}>
+                          {source.name}
+                          <br/>
+                          Section: {source.section}
+                          <br/>
+                          Url: {source.url}
+                        </div>
+                        {/* <Card className="inline-block w-auto">
+                          <CardHeader>
+                            <CardTitle className="text-md">{source.name}</CardTitle>
+                            <CardDescription>{source.section}</CardDescription>
+                          </CardHeader>
+                          <CardContent>
+                            <p>{source.name}</p>
+                          </CardContent>
+                          <CardFooter>
+                            <p>{source.url}</p>
+                          </CardFooter>
+                        </Card> */}
+                      </>
+                    )})}
+                  </ScrollArea>
+                </div>
+
+                {/* {[0,1,2].map((source, index) => {
+                return (<>
+                  <div style={{
+                    // backgroundColor:"#9abcde",
+                    padding:"10px",
+                    margin:"0px 20px 0px 0px",
+                    borderRadius:"10px",
+                    border:"1px solid #333"
+                  }}>{source}</div>
+                </>)
+              })} */}
               </fieldset>
             {/* </form> */}
+
+            
           </div>
         </main>
       </div>
