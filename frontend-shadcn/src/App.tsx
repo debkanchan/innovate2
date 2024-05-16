762e421e0857972adbec59c8d8c354bbf8c64093
Debkanchan Samadder <debu.samadder@gmail.com>
2024-05-16 17:18:24 +0530

    shadcn ui

---
+++ frontend-shadcn/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Dashboard } from './screens/dashboard';
+
+function App() {
+  return (
+    <Dashboard/>
+  );
+}
+
+export default App;
